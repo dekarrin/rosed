9ae0551650ba90eae579ac6d8c9a71aa1cbea5c6
Becky Nelson <dekarrin.irl@gmail.com>
2021-02-22 07:51:40 -0600

    wrote test for collapseSpace manipulation

--- manip_test.go
+++ manip_test.go
@@ -7,6 +7,41 @@ import (
 	"github.com/dekarrin/rosed/internal/gem"
 )
 
+func Test_Manip_collapseSpace(t *testing.T) {
+	testCases := []struct {
+		name   string
+		input  gem.String
+		sep    gem.String
+		expect gem.String
+	}{
+		{"empty input (blank)", gem.Zero, gem.Zero, gem.Zero},
+		{"empty input (nil)", nil, gem.Zero, gem.Zero},
+		{"no space to collapse", _g("no_spaces"), gem.Zero, _g("no_spaces")},
+		{"one space mid-text is not collapsed (odd runecount)", _g("testA testB"), gem.Zero, _g("testA testB")},
+		{"one space mid-text is not collapsed (even runecount)", _g("test testB"), gem.Zero, _g("test testB")},
+		{"one space at start is not collapsed (odd runecount)", _g(" test"), gem.Zero, _g(" test")},
+		{"one space at start is not collapsed (even runecount)", _g(" testA"), gem.Zero, _g(" testA")},
+		{"one space at end is not collapsed (odd runecount)", _g("test "), gem.Zero, _g("test ")},
+		{"one space at end is not collapsed (even runecount)", _g("testA "), gem.Zero, _g("testA ")},
+		{"one space everywhere is not collapsed (odd runecount)", _g(" testA testB "), gem.Zero, _g(" testA testB ")},
+		{"one space everywhere is not collapsed (even runecount)", _g(" test testB "), gem.Zero, _g(" test testB ")},
+		{"non-spacechar whitespace is converted to space (odd runecount)", _g("testA\u0085testB"), gem.Zero, _g("testA testB")},
+		{"non-spacechar whitespace is converted to space (even runecount)", _g("test\u0085testB"), gem.Zero, _g("test testB")},
+		{"ws run is collapsed (spacechar)", _g("       testA  testB  "), gem.Zero, _g(" testA testB ")},
+		{"ws run is collapsed (mixed ws)", _g("\u205f\u202ftestA\u200a  \t\n testB\t"), gem.Zero, _g(" testA testB ")},
+		{"non-ws separator", _g("testA\n  testB <SEP>\u205f  testC"), _g("<SEP>"), _g("testA testB testC")},
+		{"ws separator", _g("testA\n  testB <SEP>\u205f  testC"), _g("<SEP>"), _g("testA testB <SEP> testC")},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assertion.New(t)
+			actual := collapseSpace(tc.input, tc.sep)
+			assert.Equal(tc.expect, actual)
+		})
+	}
+}
+
 func Test_Manip_Wrap(t *testing.T) {
 	testCases := []struct {
 		name     string
