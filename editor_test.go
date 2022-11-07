8200d8637d8f1039cd6d28ebff961f1c6388463d
Deka Jello <dekarrin.irl@gmail.com>
2022-11-07 09:21:23 -0600

    Add unit tests for Editor.WithOptions to close #19

--- editor_test.go
+++ editor_test.go
@@ -27,3 +27,61 @@ func Test_Edit(t *testing.T) {
 		})
 	}
 }
+
+func Test_Editor_WithOptions(t *testing.T) {
+	testCases := []struct{
+		name string
+		ed Editor
+		opts Options
+		expect Editor
+	}{
+		{
+			name: "no prior options",
+			ed: Editor{
+				Text: "",
+				Options: Options{},
+			},
+			opts: Options{
+				LineSeparator: "x",
+			},
+			expect: Editor{
+				Text: "",
+				Options: Options{
+					LineSeparator: "x",
+				},
+			},
+		},
+		{
+			name: "all options are replaced by default",
+			ed: Editor{
+				Text: "test",
+				Options: Options{
+					ParagraphSeparator:       "PP",
+					LineSeparator:            "x",
+					IndentStr:                "             ",
+					NoTrailingLineSeparators: true,
+					PreserveParagraphs:       true,
+				},
+			},
+			opts: Options{
+				LineSeparator: "\n",
+			},
+			expect: Editor{
+				Text: "test",
+				Options: Options{
+					LineSeparator: "\n",
+				},
+			},
+		},
+	}
+	
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+			
+			actual := tc.ed.WithOptions(tc.opts)
+			
+			assert.Equal(tc.expect, actual)
+		})
+	}
+}
