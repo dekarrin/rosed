06ff43bfc2013811509164cfb0dfffbd833867ba
dekarrin <dekarrin.irl@gmail.com>
2022-12-06 06:19:04 -0600

    Made interface implementations clear for block for #16

--- block.go
+++ block.go
@@ -7,14 +7,19 @@ import (
 	"github.com/dekarrin/rosed/internal/gem"
 )
 
-// block is a "block" of text, that is some number of lines. The line separator
-// and Trailing Separator behavior are configurable, but if a constructor
-// function is used, they may be automatically detected.
+// block holds lines of text in a line-terminator agnostic way and provides a
+// way to operate on each individually, independent of what the original
+// line terminator behavior is.
+//
+// The LineSeparator and TrailingSeparator properties can be manually set,
+// but if newBlock is used to create a block, they may be automatically detected.
 //
 // Blocks are mutable and are not thread-safe.
 //
-// The Zero-value for Block is a block with no lines, with LineSeparator unset
-// and TrailingSeparator set to false.
+// The Zero-value for Block is safe to use and is a block with no lines, with
+// LineSeparator unset and TrailingSeparator set to false.
+//
+// block implements the sort.Interface interface.
 type block struct {
 	Lines             []gem.String
 	LineSeparator     gem.String
@@ -47,13 +52,13 @@ func (tb *block) AppendEmpty(count int) {
 // Apply performs the given transformation on each line and applies the results
 // to its list of lines. Lines may be both added and removed this way.
 //
-// It is important to note that the Block will always have at least one line,
-// even in the case of a Block with no content added (the line will just be
-// empty). In this case, the transform function will be called once with an
-// empty string as its argument.
+// If called on a block with empty lines, the transform function will be called
+// once with an empty string as its argument, allowing a caller to use the
+// LineOperation to produce initial text.
 //
-// All lines should be assumed to not have line terminators, and none should be
-// added.
+// All lines received by `transform` should be assumed to not have line
+// terminators, and none should be added by the `transform` function.
+// TODO: put above note on LineOperation
 func (tb *block) Apply(transform LineOperation) {
 	var applied []gem.String
 
@@ -71,7 +76,7 @@ func (tb block) CharCount(linePos int) int {
 }
 
 // Equal checks whether this Block is equal to another object. Returns whether
-// other is also a Block with the same contents as tb.
+// `other` is also a block with the same contents as tb.
 func (tb block) Equal(other interface{}) bool {
 	b2, ok := other.(block)
 	if !ok {
@@ -105,7 +110,9 @@ func (tb block) Equal(other interface{}) bool {
 }
 
 // Join converts the block into a single string by appending each line to the
-// previous with the line separator given at construction.
+// previous. Its currently-set LineSeparator is placed between each line and
+// if and only if TrailingSeparator is set, the LineSeparator is placed at the
+// end of the string as well.
 func (tb block) Join() gem.String {
 	if tb.Len() < 1 {
 		if tb.TrailingSeparator {
@@ -122,23 +129,25 @@ func (tb block) Join() gem.String {
 	return str
 }
 
-// Len gives the number of lines in the block.
+// Len gives the number of lines in the block. This is part of the implementation of
+// sort.Interface.
 func (tb block) Len() int {
 	return len(tb.Lines)
 }
 
-// Less reports whether line i should sort before line j.
+// Less reports whether line i should sort before line j. This is part of the implementation of
+// sort.Interface.
 func (tb block) Less(i, j int) bool {
 	return tb.Line(i).Less(tb.Line(j))
 }
 
-// Line gives the line at a position. pos is not checked for validity before
+// Line returns the line at the given position. `pos` is not checked for validity before
 // accessing; callers must do so.
 func (tb block) Line(pos int) gem.String {
 	return tb.Lines[pos]
 }
 
-// Remove removes a line. If pos does not exist, no action is
+// Remove removes the line at the given position. If pos does not exist, no action is
 // taken.
 func (tb *block) Remove(pos int) {
 	if pos >= 0 && len(tb.Lines) > pos {
@@ -158,14 +167,15 @@ func (tb block) String() string {
 	return fmt.Sprintf("<block LineSeparator:%q TrailingSeparator:%v Lines:%q>", tb.LineSeparator, tb.TrailingSeparator, tb.Lines)
 }
 
-// Swap swaps line i with line j.
+// Swap swaps line i with line j. This is part of the implementation of
+// sort.Interface.
 func (tb block) Swap(i, j int) {
 	tb.Lines[i], tb.Lines[j] = tb.Lines[j], tb.Lines[i]
 }
 
 // newBlock creates a block of text.
 //
-// If text is present, it is split into lines using the provided lineSep. If any
+// `text` is split into lines using the provided lineSep. If any
 // trailing line separator is present, it will be removed prior to split.
 //
 // The returned Block will contain the lines that were obtained from text. If
