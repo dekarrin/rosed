393ae0c4464efcbc00aac962ee62c12813114d82
dekarrin <dekarrin.irl@gmail.com>
2022-12-08 10:33:09 -0600

    Run gofmt

--- example_test.go
+++ example_test.go
@@ -4,19 +4,19 @@ import "fmt"
 
 func ExampleEdit() {
 	ed := Edit("sample text")
-	
+
 	fmt.Println(ed.Text)
 	// Output: sample text
 }
 
-func ExampleEditor_Apply() {	
+func ExampleEditor_Apply() {
 	namerFunc := func(lineIdx int, line string) []string {
-		newStr := fmt.Sprintf("Alpha Kid #%d: %s", lineIdx + 1, line)
+		newStr := fmt.Sprintf("Alpha Kid #%d: %s", lineIdx+1, line)
 		return []string{newStr}
 	}
-	
+
 	ed := Edit("Jane\nJake\nDirk\nRoxy").Apply(namerFunc)
-	
+
 	fmt.Println(ed.String())
 	// Output:
 	// Alpha Kid #1: Jane
@@ -35,21 +35,21 @@ func ExampleEditor_ApplyOpts() {
 		if line == "" {
 			return []string{"Nobody here!"}
 		}
-		
+
 		alphaKids++
 		newStr := fmt.Sprintf("Alpha Kid #%d: %s", alphaKids, line)
 		return []string{newStr}
 	}
-	
+
 	opts := Options{
 		NoTrailingLineSeparators: true,
-		LineSeparator: "<br/>",
+		LineSeparator:            "<br/>",
 	}
-	
+
 	// have a trailing line separator so we get the extra call of our func
 	// due to NoTrailingLineSeparators
 	ed := Edit("Jane<br/><br/>Dirk<br/>Roxy<br/>").ApplyOpts(namerFunc, opts)
-	
+
 	fmt.Println(ed.String())
 	// Output:
 	// Alpha Kid #1: Jane<br/>Nobody here!<br/>Alpha Kid #2: Dirk<br/>Alpha Kid #3: Roxy<br/>Nobody here!
