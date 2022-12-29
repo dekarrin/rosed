2ad733ec8f7e1a6a16cd5ef2bb1b25e5c14b33ee
dekarrin <dekarrin.irl@gmail.com>
2022-12-29 10:48:50 -0600

    #109: stubbed out InsertTable and created all Options keys that will be needed

--- operations.go
+++ operations.go
@@ -563,6 +563,28 @@ func (ed Editor) InsertDefinitionsTableOpts(pos int, definitions [][2]string, wi
 	}
 }
 
+// InsertTable creats a table from provided data and inserts it into the text of
+// the Editor.
+//
+// This function is affected by the following [Options]:
+//
+//   - LineSeparator is used to separate each line of the output.
+//   - NoTrailingLineSeparators sets whether to include a trailing LineSeparator
+//     at the end of the generated table. If set to true, it will be omitted,
+//     otherwise the table will end with a LineSeparator.
+//   - TableBorders controls whether the table will have a border.
+//   - TableHeaders controls whether the first line of data is treated as
+//     headers.
+//   - TableCharSet gives the characters to use to represent lines in the table,
+//     used for drawing borders. If TableBorders is enabled, the characters in
+//     TableCharSet are used to draw the borders. If TableBorders is disabled
+//     but TableHeaders is enabled, the characters in TableCharSet are used to
+//     draw the horizontal rule separating the headers from the data.
+func (ed Editor) InsertTable(pos int, data [][]string, width int) Editor {
+
+	return ed
+}
+
 // InsertTwoColumns builds a two-column layout of side-by-side text from two
 // sequences of text and inserts it into the text of the Editor. The leftText
 // and the rightText do not need any special preparation to be used as the body
